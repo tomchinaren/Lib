4139ac041c0845d24c1447b102c784c68d4feeca
liangjuntang <liangjuntang@mallcoo.cn>
2020-07-23 18:40:02 +0800

    TaskObserver V2

---
+++ src/Tom.Lib/ObserversV2/AbstractTaskObserver.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tom.Lib.ObserversV2
+{
+    /// <summary>
+    /// 可开启定时执行的订阅者，支持设置observalbe以便能主动拉取消息
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class AbstractTaskObserver<T> : IObserver<T>
+    {
+        // 观察者，用于主动拉取数据
+        DefaultObservable<T> observable;
+
+        public AbstractTaskObserver(DefaultObservable<T> observable)
+        {
+            this.observable = observable;
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(T value)
+        {
+            // 忽略通知，使用定时异步出列
+        }
+
+
+        /// <summary>
+        /// 开启定时执行
+        /// </summary>
+        public void StartAutoRun()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(3));
+
+            var task = Task.Run(() => {
+                while (true)
+                {
+                    try
+                    {
+                        while (observable.TryDequeue(out T data))
+                        {
+                            HandleMessage(data);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        HandleDequeueException(ex);
+                    }
+
+                    // 队列没有数据时，休眠1秒后继续尝试出列
+                    Thread.Sleep(1000);
+                }
+            });
+        }
+
+        protected abstract void HandleMessage(T message);
+
+        protected abstract void HandleDequeueException(Exception ex);
+
+    }
+
+}
---
+++ src/Tom.Lib/ObserversV2/DefaultObservable.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Tom.Lib.ObserversV2
+{
+    /// <summary>
+    /// 基于本地队列的观察者
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class DefaultObservable<T> : IObservable<T>
+    {
+        // 订阅者列表
+        public List<IObserver<T>> observers = new List<IObserver<T>>();
+
+        // 本地消息队列
+        ConcurrentQueue<T> messageQueue = new ConcurrentQueue<T>();
+
+        /// <summary>
+        /// 订阅
+        /// </summary>
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            observers.Add(observer);
+
+            return new UnSubscriber(observers, observer);
+        }
+
+
+        /// <summary>
+        /// 发布消息
+        /// </summary>
+        public void Publish(T message)
+        {
+            // 入列
+            Enqueue(message);
+
+            // 通知订阅者
+            Notify(message);
+        }
+
+        /// <summary>
+        /// 通知订阅者
+        /// </summary>
+        protected virtual void Notify(T message)
+        {
+            foreach (var observer in observers)
+            {
+                observer.OnNext(message);
+            }
+        }
+
+        // 入列
+        protected virtual void Enqueue(T message)
+        {
+            messageQueue.Enqueue(message);
+        }
+
+        /// <summary>
+        /// 出列
+        /// </summary>
+        /// <returns></returns>
+        public virtual bool TryDequeue(out T result)
+        {
+            return messageQueue.TryDequeue(out result);
+        }
+
+        /// <summary>
+        /// 获取当前队列长度，用于监控队列以免过大
+        /// </summary>
+        /// <returns></returns>
+        public int GetQueueLength()
+        {
+            return messageQueue.Count;
+        }
+
+
+        // 内部类，用于Subscribe方法返回IDisposable
+        private class UnSubscriber : IDisposable
+        {
+            public List<IObserver<T>> observers;
+            public IObserver<T> observer;
+
+            public UnSubscriber(List<IObserver<T>> observers, IObserver<T> observer)
+            {
+                this.observers = observers;
+                this.observer = observer;
+            }
+
+            public void Dispose()
+            {
+                if (observers != null && observers.Contains(observer))
+                {
+                    observers.Remove(observer);
+                }
+            }
+        }
+
+    }
+
+}
